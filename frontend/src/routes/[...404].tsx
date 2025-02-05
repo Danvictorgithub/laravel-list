8a8e36dee6474a4a8436b20bdf0fce0cbde27aee
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-05 17:03:08 +0800

    updated 404 page

--- frontend/src/routes/[...404].tsx
+++ frontend/src/routes/[...404].tsx
@@ -1,4 +1,5 @@
 import { Title } from "@solidjs/meta";
+import { A } from "@solidjs/router";
 import { HttpStatusCode } from "@solidjs/start";
 
 export default function NotFound() {
@@ -6,14 +7,27 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <div class="h-screen w-screen  flex items-center">
+        <div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-800">
+          <div class="max-w-md">
+            <div class="text-5xl font-dark font-bold">404</div>
+            <p class="text-2xl md:text-3xl font-light leading-normal">
+              Sorry we couldn't find this page.{" "}
+            </p>
+            <p class="mb-8">
+              But dont worry, you can find plenty of other things on our
+              homepage.
+            </p>
+
+            <A href="/">
+              <button class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
+                back to homepage
+              </button>
+            </A>
+          </div>
+          <div class="max-w-lg"></div>
+        </div>
+      </div>
     </main>
   );
 }
