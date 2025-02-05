91df539f0ac806f5820ab35787aa4659fef09ecc
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-05 17:03:02 +0800

    created landing page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,23 @@
-import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
-
+import { A } from "@solidjs/router";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <section class="h-svh flex items-center justify-center flex-col text-gray-800">
+        <div>
+          <h1 class="font-bold text-7xl">Taskmone</h1>
+          <p>Your ultimate solution for a productivity app</p>
+        </div>
+        <div class="mt-12">
+          <A href="/auth/signin">
+            <button
+              class="border border-black px-4 py-2 rounded-xl hover:bg-black hover:text-white hover:border-white active:text-black   "
+              type="button"
+            >
+              sign in
+            </button>
+          </A>
+        </div>
+      </section>
     </main>
   );
 }
