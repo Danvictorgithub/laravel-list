4d562d8f88ab329d189339a0540f808332fb50ca
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-05 22:26:34 +0800

    converted back to tailwind 3.0 because of lack of shadcn support

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,7 +1,2 @@
 import { defineConfig } from "@solidjs/start/config";
-import tailwind from "@tailwindcss/vite";
-export default defineConfig({
-  vite: {
-    plugins: [tailwind() as any],
-  },
-});
+export default defineConfig({});
